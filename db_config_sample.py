09ff609923fa0cff0d0710ee648eaea70825794d
Michael Fekadu <mike.fekadu@gmail.com>
2019-11-08 13:09:04 -0800

    Create db_config_sample.py

---
+++ db_config_sample.py
@@ -0,0 +1,8 @@
+# global dictionary should be a secret file
+CONFIG = {
+    'host': '<DB_IP_ADDRESS>',
+    'user': 'username',
+    'password': 'password',
+    'sql_dir': 'sql',
+    'create_file': 'csai_nov_8_2019_create_script.min.sql'
+}
