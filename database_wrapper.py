25fe9ddfc2cd12c6b797971b69623ff6d635fe8c
Michael Fekadu <mike.fekadu@gmail.com>
2020-01-10 18:56:28 -0800

    adds NimbusDatabase template method w/ MySQL implement
    (venv) ➜  api git:(mf-flask-api) ✗ python3 demo.py
    Entites: ['Clubs', 'Corequisites', 'Corrections', 'Courses', 'OfficeHours', 'PolyRatings', 'Prerequisites', 'Professors', 'QuestionAnswerFormats', 'ResearchInterests', 'Sessions', 'ShortNames']
    Unique departments?: ['CPE', 'CSC']
    How many terms is CSC 100 offered?: 1
    database object is being garbage collected...
    database connection was closed.
    (venv) ➜  api git:(mf-flask-api) ✗
    
    ================
    
    https://refactoring.guru/design-patterns/template-method/python/example

--- database_wrapper.py
+++ database_wrapper.py
@@ -5,56 +5,13 @@ different databases and storage locations.
 
     Typical usage example:
 
-    connection = connect_to_mysql('config.json')
-    bar = foo.FunctionBar()
+    db = NimbusMySQL(config_file="config.json")
+    ents = db.get_entities()
 """
 #!/usr/bin/env python3
 import mysql.connector
+from abc import ABC, abstractmethod
 import json
-
-MYSQL_TYPE = mysql.connector.connection_cext.CMySQLConnection
-
-
-class MySQLAdapter:
-    """An adapter for mysql-connector-python to fit our program.
-
-    The MySQLAdapter makes the mysql-connector-python interface
-    compatible with the our program's interface.
-
-    Attributes:
-        connection: The mysql-connector-python connection object.
-        database: the database to execute queries on.
-    """
-
-    def __init__(self,
-                 mysql_connection_object: MYSQL_TYPE,
-                 database: str) -> None:
-        """Inits MySQLAdapter with mysql_connection_object.
-
-        Args:
-            mysql_connection_object: object representing a mysql connection.
-            database: the database to execute queries on.
-
-        Returns:
-            None
-        """
-        self.connection = mysql_connection_object
-        self.database = database
-
-    def get_entities(self) -> str:
-        """Yields a list of all entities in the database."""
-        cursor = self.connection.cursor()
-        cursor.execute('use {}'.format(self.database))
-        cursor.execute('show tables')
-        # `fetchall` returns a list of single element tuples
-        tups = cursor.fetchall()
-        cursor.close()
-        for x in tups:
-            yield x[0]
-
-    def get_relationships(self) -> str:
-        """Returns a list of all relationships between entities in database."""
-        pass
 
 
 class UnsupportedDatabaseError(Exception):
@@ -68,43 +25,180 @@ class UnsupportedDatabaseError(Exception):
         self.message = message
 
 
-def connect(database_type: str = 'mysql', config_file: str = 'config.json'):
-    """Returns the connection object after connecting to a database
+class BadConfigFileError(Exception):
+    """Raised when the config.json file is badly formatter (e.g missing field).
 
-    Args:
-        database_type: An optional variable for choosing the database. (default=420)
-        config_file: the filename string of the config file. (default=
-
-    Returns:
-        a connection object
+    Attributes:
+        message: an explanation.
     """
-    if database_type:
-        return connect_to_mysql(config_file)
-    else:
-        return None  # ???
+
+    def __init__(self, message: str):
+        self.message = message
 
 
-def connect_to_mysql(config_file):
-    """Connect to a MySQL database given a configuration file in JSON format.
-
-    Args:
-        config_file: the config.JSON filename with a "mysql" key.
-
-    Returns:
-        the mysql-connector-python connection object
-
-    Raises:
-        UnsupportedDatabaseError: If an unknown database type found.
+class NimbusDatabase(ABC):
     """
-    with open(config_file) as json_data_file:
-        config = json.load(json_data_file)
+    An abstract class for interacting with the Nimbus database.
+    Concrete subclasses, such as NimbusMySQL,
+    should implement these operations such as `connect`
+    """
 
-    if config.get('mysql', False):
-        mysql_config = config['mysql']
-        return mysql.connector.connect(
-            host=mysql_config['host'],
-            user=mysql_config['user'],
-            passwd=mysql_config['password']
-        )
-    else:
-        raise UnsupportedDatabaseError("sorry, idk this database type.")
+    def __init__(self, config_file: str = 'config.json') -> None:
+        """
+        Inits Nimbus Database using the hostname, username, password
+        found inside the config_file.
+        """
+        pass
+
+    @abstractmethod
+    def get_entities(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_unique(self, entity) -> str:
+        pass
+
+    @abstractmethod
+    def get_bitcount(self, entity) -> str:
+        pass
+
+    @abstractmethod
+    def close(self) -> None:
+        """
+        Simple Implementation Example:
+        ```
+        self.connection.close()
+        super().close()
+        ```
+        """
+        print("database connection was closed.")
+        pass
+
+    def __del__(self) -> None:
+        """
+        This method can make sure that the database connection is closed
+        before garbage references are collected.
+
+        There are reasons to not use `__del__`:
+        https://stackoverflow.com/q/1481488
+
+        Example:
+            >>> import database_wrapper
+            >>> db = database_wrapper.NimbusMySQL()
+            >>> del db
+            database object is being garbage collected...
+            database connection was closed.
+        """
+        print("database object is being garbage collected...")
+        self.close()
+        return
+
+
+class NimbusMySQL(NimbusDatabase):
+    """An adapter for mysql-connector-python to fit our program.
+
+    The NimbusMySQL makes the mysql-connector-python interface
+    compatible with the our program's interface.
+
+    Attributes:
+        config_file: a JSON file with the mysql details.
+    """
+
+    def __init__(self, config_file: str = 'config.json') -> None:
+        """
+        Inits Nimbus Database using the hostname, username, password
+        found inside the config_file.
+
+        Args:
+            config_file: a JSON file with a 'mysql' object that holds
+            the connection details.
+
+        Returns:
+            None
+
+        Raises:
+            BadConfigFileError: If the config_file fields are unexpected.
+        """
+        self.connection = None  # gets set according to config_file
+        self.database = None  # gets set according to config_file
+
+        with open(config_file) as json_data_file:
+            config = json.load(json_data_file)
+
+        if config.get('mysql', False):
+            mysql_config = config['mysql']
+            self.connection = mysql.connector.connect(
+                host=mysql_config['host'],
+                user=mysql_config['user'],
+                passwd=mysql_config['password']
+            )
+
+            self.database = mysql_config['database']
+
+            if self.connection is None or self.database is None:
+                raise BadConfigFileError('failed to connect to MySQL')
+        else:
+            msg = "config.json is missing {} field.".format('mysql')
+            raise BadConfigFileError(msg)
+
+    def yield_entities(self) -> str:
+        """Yields a list of all entities in the database."""
+        cursor = self.connection.cursor()
+        cursor.execute('use {}'.format(self.database))
+        cursor.execute('show tables')
+        # `fetchall` returns a list of single element tuples
+        tups = cursor.fetchall()
+        cursor.close()
+        for x in tups:
+            yield x[0]
+
+    def get_entities(self) -> str:
+        """
+        Returns a list of all entities in the database.
+
+        Example:
+        >>> from database_wrapper import NimbusMySQL
+        >>> db = NimbusMySQL()
+        >>> db.get_entities()
+        ['Clubs', 'Corequisites', 'Corrections', 'Courses', 'OfficeHours',
+        'PolyRatings', 'Prerequisites', 'Professors', 'ResearchInterests',
+        'ResponseFormats', 'ShortNames']
+        """
+        cursor = self.connection.cursor()
+        cursor.execute('use `{}`'.format(self.database))
+        cursor.execute('show tables')
+        # `fetchall` returns a list of single element tuples
+        tups = cursor.fetchall()
+        cursor.close()
+        return [x[0] for x in tups]
+
+    def get_relationships(self) -> str:
+        """Returns a list of all relationships between entities in database."""
+        pass
+
+    def get_unique(self, entity, prop) -> str:
+        """
+        """
+        cursor = self.connection.cursor()
+        cursor.execute('use `{}`'.format(self.database))
+        cursor.execute('select distinct({}) from {}'.format(prop, entity))
+        # `fetchall` returns a list of single element tuples
+        tups = cursor.fetchall()
+        cursor.close()
+        return [x[0] for x in tups]
+
+    def get_bitcount(self, entity, prop) -> str:
+        """
+        """
+        cursor = self.connection.cursor()
+        cursor.execute('use `{}`'.format(self.database))
+        cursor.execute('select bit_count(`{}`) from `{}`'.format(prop, entity))
+        # `fetchall` returns a list of single element tuples
+        tups = cursor.fetchall()
+        cursor.close()
+        return [x[0] for x in tups]
+
+    def close(self) -> None:
+        """Close the database connection"""
+        self.connection.close()
+        super().close()
