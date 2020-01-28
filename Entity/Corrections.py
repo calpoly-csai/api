4f89c6284ff9feef676c28979a1d9e0bc26b8bdd
taylor-nguyen-987 <43256137+taylor-nguyen-987@users.noreply.github.com>
2020-01-27 21:54:11 -0800

    Corrections.py

---
+++ Entity/Corrections.py
@@ -0,0 +1,8 @@
+class Corrections:
+    def __init__(self, correctionId, rawText, correctionText,
+                 tableNameToCorrect, columnNameToCorrect):
+        self.correctionId=correctionId
+        self.rawText=rawText
+        self.correctionText=correctionText
+        self.tableNameToCorrect=tableNameToCorrect
+        self.columnNameToCorrect=columnNameToCorrect
