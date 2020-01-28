abbd1e795f206a1e20b6f7a322682ec6bbfee305
taylor-nguyen-987 <43256137+taylor-nguyen-987@users.noreply.github.com>
2020-01-27 21:59:39 -0800

    Courses.py

---
+++ Entity/Courses.py
@@ -0,0 +1,16 @@
+class Courses:
+    def __init__(self, courseId, dept, courseNum, termsOffered, units, courseName,
+                 raw_concurrent_text, raw_recommended_text, raw_prerequisites_text, crossListedAs,
+                 raw_standing_text, standing):
+        self.courseId=courseId
+        self.dept=dept
+        self.courseNum=courseNum
+        self.termsOffered=termsOffered
+        self.units=units
+        self.courseName=courseName
+        self.raw_concurrent_text=raw_concurrent_text
+        self.raw_recommended_text=raw_recommended_text
+        self.raw_prerequisites_text=raw_prerequisites_text
+        self.crossListedAs=crossListedAs
+        self.raw_standing_text=raw_standing_text
+        self.standing=standing
