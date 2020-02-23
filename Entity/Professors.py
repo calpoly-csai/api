2ba57d5339beff5f4d5bb7fcb5b1a274980ac0e0
danield2255 <danield2255@gmail.com>
2020-02-23 14:07:13 -0800

    SqlAlchemy usaeble Professor type

--- Entity/Professors.py
+++ Entity/Professors.py
@@ -1,10 +1,15 @@
-class Professors:
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.ext.declarative import declarative_base
 
-    def __init__(self, id, firstName, lastName, phoneNumber, researchInterests,
-                 email):
-        self.id = id
-        self.firstName = firstName
-        self.lastName = lastName
-        self.phoneNumber = phoneNumber
-        self.researchInterests = researchInterests
-        self.email = email
+
+# This is the way SQLAlchemy initializes their special classes
+Base = declarative_base()
+
+class Professors(Base):
+    __tablename__ = 'Professors'
+    id = Column(Integer, primary_key=True)
+    firstName = Column(String(50))
+    lastName = Column(String(50))
+    phoneNumber = Column(String(20))
+    researchInterests = Column(Text)
+    email = Column(String(255))
