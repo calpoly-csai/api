9f7ea8ec9276cfc8469094deefdc44ec3d894d0f
Michael Fekadu <mike.fekadu@gmail.com>
2020-02-23 22:24:08 -0800

    Revert "Added club_name field to Club entity and changed variables to follow pep8 convention"
    This reverts commit b3a801a08b2701ad9cfc3d3971e8a34bbb08dcb7.

--- Entity/Clubs.py
+++ Entity/Clubs.py
@@ -1,45 +1,5 @@
-from builtins import str
-
-from sqlalchemy import Column, Integer, String, Text, Enum,
-from sqlalchemy.ext.declarative import declarative_base
-import enum
-from sqlalchemy.dialects.mysql import SET
-
-'''
-Column types of 'Clubs' table gathered from SQLWorkbench
-
-idClubs int(11) AI PK
-types varchar(255)
-desc text
-contactEmail varchar(45)
-contactEmail2 varchar(45)
-contactPerson varchar(45)
-contactPhone varchar(15)
-box varchar(45)
-advisorId int(11) #foreign id 
-affiliation varchar(255)
-'''
-
-Base = declarative_base()
-
-class Clubs(enum.Enum):
-    __tablename__ = "Clubs"
-    id_clubs = Column(Integer, primary_key = True)
-    club_name = Column(String(512)) #Added club_name
-    types = Column(String(255))
-    desc = Column(Text)
-    contact_email = Column(String(45))
-    contact_email_2 = Column(String(45))
-    contact_person = Column(String(45))
-    contact_phone = Column(String(15))
-    box = Column(String(45))
-    advisor_id = Column(Integer)
-    affiliation = Column(String(255))
-
-    def __repr__(self):
-        return "<Clubs (id = {}, "
-'''
 class Clubs:
+
     def __init__(self, idClubs, types, desc, contactEmail, contactEmail2,
                  contactPerson, contactPhone, box, advisorId, affiliation):
         self.idClubs = idClubs
@@ -52,4 +12,3 @@ class Clubs:
         self.box = box
         self.advisorId = advisorId
         self.affiliation = affiliation
-'''
