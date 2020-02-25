423c1c2da15670a55524b457366dff17985ad5a5
Michael Fekadu <mfekadu@users.noreply.github.com>
2020-02-25 14:11:54 -0800

    using a better auto formatter
    black.readthedocs.io

--- format.sh
+++ format.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-
-# --recursive: makes it work on folders
-# --versbose: prints out the filename(s) while running 
-# --in-place: actually modifies the file(s)
-yapf --recursive --verbose --in-place *.py Entity/*.py modules/*.py
+black .
