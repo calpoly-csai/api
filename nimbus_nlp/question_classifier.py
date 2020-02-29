6c7c45a38c30be88f103c695fb0c34db035285cc
Adam Perlin <adamp@nanosoft.com>
2020-02-28 17:26:46 -0800

    Fix for scikit-learn version and nimbus_nlp model imports

--- nimbus_nlp/question_classifier.py
+++ nimbus_nlp/question_classifier.py
@@ -6,7 +6,7 @@ import sklearn.neighbors
 import pandas as pd
 import sys
 import json
-from nimbus_nlp.save_and_load_model import save_model, load_latest_model, PROJECT_DIR
+from save_and_load_model import save_model, load_latest_model, PROJECT_DIR
 
 # TODO: move the functionality in this module into class(es), so that it can be more easily used as a dependency
 
