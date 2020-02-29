9e025d82b8333c8cba6e8c0e8941b761954738f2
Miles Aikens <snekiam@gmail.com>
2020-02-28 17:38:13 -0800

    Fix gunicorn_config to be compatible with Heroku virtualization

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -7,4 +7,4 @@ DEBUG_MODE = int(environ.get("DEBUG_MODE", 1))
 # Gunicorn config
 bind = ":" + str(PORT)
 workers = multiprocessing.cpu_count() * 2 + 1
-threads = 2 * multiprocessing.cpu_count()
+threads = 3
