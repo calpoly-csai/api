dc3922041e62606f8c380c4a6ed554d90fb207f5
Cameron Toy <cameron-toy@users.noreply.github.com>
2020-02-28 18:50:24 -0800

    downloads stopwords corpus

--- nimbus_nlp/question_classifier.py
+++ nimbus_nlp/question_classifier.py
@@ -12,6 +12,7 @@ from nimbus_nlp.save_and_load_model import save_model, load_latest_model, PROJEC
 
 class QuestionClassifier:
     def __init__(self):
+        nltk.download('stopwords')
         self.classifier = None
         self.nlp = spacy.load('en_core_web_sm')
         self.WH_WORDS = {'WDT', 'WP', 'WP$', 'WRB'}
