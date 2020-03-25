df611f170d87ea840d330fe437026cf5fe5f1dbe
Michael Fekadu <mike.fekadu@gmail.com>
2020-03-25 16:45:01 -0700

    pipenv install --dev coverage

--- coverage.sh
+++ coverage.sh
@@ -3,5 +3,5 @@ python -m coverage run -m pytest
 # omit because pipenv creates a "~/.local" directory
 coverage html --omit="*.local*"
 
-# open htmlcov/index.html
+echo "\nopen htmlcov/index.html\n"
 
