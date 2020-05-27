71c2175ac6f77bc2214686fc1d7ca0d641f51f7b
Michael Fekadu <mike.fekadu@gmail.com>
2020-05-27 12:51:19 -0700

    delete dead scripts

--- download_nlp_stuff.sh
+++
@@ -1,5 +0,0 @@
-#
-# This script will download NLP stuff.
-#
-#
-python3 -m spacy download en_core_web_sm
--- download_nltk_stuff.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import nltk
-
-nltk.download("punkt")
-nltk.download("averaged_perceptron_tagger")
